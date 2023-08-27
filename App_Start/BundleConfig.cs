f4cb30444057f8d1e4c70f4ba0c9f75ea892d11b
Roth <andres.mesa1707@gmail.com>
2023-08-26 21:47:36 -0400

    BugFix Bundle

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,8 +8,9 @@ namespace KrosfyPay
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+
+            //bundles.add(new scriptbundle("~/bundles/jquery").include(
+            //            "~/scripts/jquery-{version}.js"));
 
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
             //"~/Scripts/jquery.validate*"));
@@ -19,29 +20,33 @@ namespace KrosfyPay
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Content/assets/css/bootsnav.css"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/assets/css/style.css"));
+                      "~/Content/assets/css/style.css",
+                      "~/Content/assets/css/bootsnav.css",
+                      "~/Content/assets/lib/owlcarousel/assets/owl.carousel.min.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Content/assets/js/animations.js",
                         "~/Content/assets/js/main.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/url/https://code.jquery.com/jquery-3.4.1.min.js",
-            //            "~/url/https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Content/assets/js/jquery-3.4.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Content/assets/js/bootstrap.bundle.min.js.map"));
 
             bundles.Add(new ScriptBundle("~/bundles/jsLibrary").Include(
                         "~/Content/assets/lib/easing/easing.min.js",
                         "~/Content/assets/lib/waypoints/waypoints.min.js",
-                        "~/Content/assets/lib/counterup/counterup.min.js",
+                        "~/Content/assets/lib/counterup/jquery.counterup.js",
+                        "~/Content/assets/lib/counterup/jquery.counterup.min.js",
                         "~/Content/assets/lib/owlcarousel/owl.carousel.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/mail").Include(
                         "~/Content/assets/mail/jqBootstrapValidation.min.js",
                         "~/Content/assets/mail/contact.js"));
+
+
 
         }
     }
